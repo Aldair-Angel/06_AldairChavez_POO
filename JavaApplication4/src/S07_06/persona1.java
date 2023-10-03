5e291035a9c0a2622688cb6b02976cbf01b22d48
Aldair-Angel <aldair.chavez@vallegrande.edu.pe>
2023-10-03 09:32:12 -0500

    persona 1

--- JavaApplication4/src/S07_06/persona1.java
+++ JavaApplication4/src/S07_06/persona1.java
@@ -10,5 +10,9 @@ package S07_06;
  * @author User
  */
 public class persona1 {
-    
+    public void MayorDeEdad(int edad, String nombre,float altura){
+        if(edad <= 18){
+            System.out.println("El ciudadano" + nombre + "Es mayor de edad, cuenta con una altura de," + altura);  
+        }
+    }
 }
